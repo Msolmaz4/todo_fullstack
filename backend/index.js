0ab4c5040041bc3288a8cd73ddc0ee4eaaa75eed
Muhammet <msolmaz83@gmail.com>
2024-04-20 21:39:44 +0200

    son2

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,24 @@
-const express = require("express")
-const app = express()
-const PORT = 8000
-const cors = require('cors')
-app.use(cors())
+const express = require("express");
+const app = express();
+const PORT = 8000;
+const cors = require('cors');
 
+// Tüm kaynaklara erişime izin vermek için CORS middleware'ini ayarlayın
+app.use(cors());
 
-app.use(cors({
-    origin: 'https://todo-fullstack-y4kx.vercel.app'
-  }));
-  
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-const userRouter = require("./routes/userRouter")
-const todoRouter = require("./routes/todoRouter")
-require("./db/db")
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+const userRouter = require("./routes/userRouter");
+const todoRouter = require("./routes/todoRouter");
+require("./db/db");
 
-const bodyParser = require('body-parser');
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
 
-app.get("/",(req,res)=>{
-    res.send("hello world")
-})
-app.use('/todos',todoRouter)
-app.use('/user',userRouter)
+app.use('/todos', todoRouter);
+app.use('/user', userRouter);
 
-
-app.listen(PORT,()=>{
-    console.log(`${PORT} dinleniyor`)
-})
+app.listen(PORT, () => {
+    console.log(`${PORT} dinleniyor`);
+});

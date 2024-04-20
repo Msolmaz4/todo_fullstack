f17b76c793c0e24bd7cf9087e028fca815c7baca
Muhammet <msolmaz83@gmail.com>
2024-04-20 22:55:54 +0200

    mongo

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose')
 
 
 const main = () =>{
-    mongoose.connect('mongodb+srv://deneme12:Azxs12345@cluster0.wpuxx.mongodb.net/tododersgithub') 
+    mongoose.connect('mongodb+srv://deneme12:Azxs12345@cluster0.wpuxx.mongodb.net/tododersgithub', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }) 
     .then(console.log('connect to the db success'))
     .catch(err => console.log(err))
 }
